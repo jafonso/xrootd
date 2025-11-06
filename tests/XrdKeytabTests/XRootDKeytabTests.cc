9da3abfcd57e3ec342519f1854e231da7a5b8f75
Joao Afonso <joao.afonso@cern.ch>
2025-11-06 17:38:28 +0100

    Adding new tests

---
+++ tests/XrdKeytabTests/XRootDKeytabTests.cc
@@ -0,0 +1,11 @@
+#include "XrdPfc/XrdPfcPathParseTools.hh"
+
+#include <gtest/gtest.h>
+
+class PathParseToolTest : public ::testing::Test {};
+
+TEST_F(PathParseToolTest, SplitParser)
+{
+    EXPECT_EQ(0, 0);
+}
+
