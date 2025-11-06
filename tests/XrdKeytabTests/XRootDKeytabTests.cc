a880748f57b7fb04edef15ba8e69791ec18ab789
Joao Afonso <joao.afonso@cern.ch>
2025-11-06 17:50:14 +0100

    Added test

--- tests/XrdKeytabTests/XRootDKeytabTests.cc
+++ tests/XrdKeytabTests/XRootDKeytabTests.cc
@@ -2,10 +2,17 @@
 
 #include <gtest/gtest.h>
 
+namespace {
+    constexpr FILE_PATH = "/tmp/cta-taped.sss.keytab"
+}
+
 class PathParseToolTest : public ::testing::Test {};
 
 TEST_F(PathParseToolTest, SplitParser)
 {
-    EXPECT_EQ(0, 0);
+    XrdOucErrInfo eInfo;
+    XrdSecsssKT *kTab;
+    kTab = new XrdSecsssKT(&eInfo, Opt.KeyFile, XrdSecsssKT::isAdmin);
+    EXPECT_TRUE(kTab);
 }
 
